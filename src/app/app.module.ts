fd3dbed930ae21f4e5853f2e9931814cc5488602
JavierM <144504968+jav0314@users.noreply.github.com>
2024-05-25 19:34:26 -0500

    upload local repository to GitHub

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SingupComponent } from './lib/autenticacion/singup/singup.component';
+import { LoginComponent } from './lib/autenticacion/login/login.component';
+import { EjemplosComponent } from './lib/ejemplos/ejemplos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SingupComponent,
+    LoginComponent,
+    EjemplosComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/lib/ejemplos/ejemplos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ejemplos',
+  templateUrl: './ejemplos.component.html',
+  styleUrl: './ejemplos.component.css'
+})
+export class EjemplosComponent {
+
+}
